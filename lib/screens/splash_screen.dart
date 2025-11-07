abe6fd6ba1e1edcaaca76f4a5926702d69412295
BlackEmpir7199 <blackempire9177@gmail.com>
2025-11-08 01:31:32 +0530

    ui: completed a simulated splash screen with animation

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,64 @@
+import 'package:donut_app_ui/screens/home_screen.dart';
+import 'package:donut_app_ui/utils/util.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController donutController;
+  late Animation<double> rotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    donutController = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat();
+
+    rotationAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(parent: donutController, curve: Curves.ease));
+  }
+
+  @override
+  void dispose() {
+    donutController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.of(
+        context,
+      ).pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen()));
+    });
+
+    return Scaffold(
+      backgroundColor: Utils.mainColor,
+      body: Center(
+        child: Column(
+          spacing: 35,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RotationTransition(
+              turns: rotationAnimation,
+              child: Image.network(Utils.donutLogoWhiteNoText),
+            ),
+
+            Image.network(Utils.donutLogoWhiteText),
+          ],
+        ),
+      ),
+    );
+  }
+}
