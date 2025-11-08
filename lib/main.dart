a6588568e25da3276615c09b734b27b4e80a69f2
BlackEmpir7199 <blackempire9177@gmail.com>
2025-11-08 12:40:58 +0530

    ui: creating the home screen layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:donut_app_ui/screens/home_screen.dart';
 import 'package:donut_app_ui/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home:SplashScreen()
+      home:HomeScreen()
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:donut_app_ui/utils/util.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column());
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.network(Utils.donutLogoRedText, width: 150),
+        foregroundColor: Utils.mainDark,
+        centerTitle: true,
+      ),
+      body: Column(
+        
+      ),
+      drawer: _buildDrawer(),
+    );
   }
+}
+
+
+Widget _buildDrawer() {
+  return Drawer(
+    child: Container(
+      padding: EdgeInsets.all(35),
+      color: Utils.mainDark,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(Utils.donutLogoWhiteNoText, height: 150, width: 150),
+          Image.network(Utils.donutLogoWhiteText, width: 180),
+        ],
+      ),
+    ),
+  );
 }
